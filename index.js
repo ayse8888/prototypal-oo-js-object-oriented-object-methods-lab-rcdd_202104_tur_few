7a0f1617d0a2da7b3b7f6f8b5e931f590acb048c
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-26 14:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-    // this.sayHi = function() {
-    //     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
-    // };
+    this.sayHi = function() {
+        console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
+    };
 }
 
 // User.prototype.sayHi = function() {
