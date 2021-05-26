f34b21dcdc0b7d05071c181ac611e2fef055f87b
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-26 14:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function BoardMember(name, homeState, training) {
         console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
     };
 }
+
+User.prototype.sayHello = function() {
+  console.log(`Hello everybody, my name is ${this.name}`);
+};
  
 let boardMember = new BoardMember('Mr. Polished', 'New York', 'law');
 boardMember.sayHi(); 
