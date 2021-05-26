6ca7a161ecdca2d2fdceeefab2a771d5e662905f
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-26 14:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-    this.sayHi = function() {
-        console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
-    };
-}
+//     this.sayHi = function() {
+//         console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
+//     };
+// }
 
-User.prototype.sayHello = function() {
-  console.log(`Hello everybody, my name is ${this.name}`);
+User.prototype.sayHi = function() {
+        console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`);
 };
  
 let boardMember = new BoardMember('Mr. Polished', 'New York', 'law');
